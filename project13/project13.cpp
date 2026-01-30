56de1d68623266f01e8fc8b801914414732b0359
seanlee99 <seanlee.kr99@gmail.com>
2026-01-30 12:51:55 +0900

    function

--- project13/project13.cpp
+++ project13/project13.cpp
@@ -13,17 +13,17 @@ int main()
 	// 문자열은 배열이다
 	// char sz[] = {65, 66, 67, 68, 69, 70, 0};
 	// char sz[] = {'A', 'B', 'C', 'D', 'E', 'F', '\0'};
-	// char sz[] = { 'H', 'e', 'l' , 'l', 'o', '\0'};
-	char sz[] = "Hello"; // 문자열(String)
-	int len = sizeof(sz)/ sizeof(char); //배열의 길이 구하기
+	char sz[] = { 'H', 'e', 'l' , 'l', 'o', '\0'};
+	// char sz[] = "Hello"; // 문자열(String)
+	int hi = sizeof(sz) / sizeof(char); //배열의 길이 구하기
 	/*for (int i = 0; i < len; i++)
 
 	{
 		printf("%c", sz[i]);
 	}
 	*/
-	printf("sz배열의 크기: %d\n", len);
-	printf("%s\n", sz);
+	printf("sz배열의 크기: %d\n", hi);
+	// printf("%s\n", sz);
 	// sz[1] = 'E';
 	printf("%s\n", sz);
 
@@ -35,23 +35,61 @@ int main()
 
 	char sz2[] = "Hello"; // 문자열(String)
 	//if (sz == sz2)
-	if (strcmp(sz, sz2)==0)
+	if (strcmp(sz, sz2) == 0)
 		printf("%s와 %s는 서로 같음\n", sz, sz2);
 	else
 		printf("%s와 %s는 서로 다름\n", sz, sz2);
 
 	// & : 주소연산자
 	// 문자열의 입력
-	char name[10]; // 알파벳 9글자까지 입력 가능
+	char name[20];
 	printf("이름을 입력하세요: ");
 	// scanf()는 공백문자를 포함한 문자열을 입력받을 수 없다
-	scanf("%s", name); // 문자열을 입력받을때는 주소연산자를 붙이지 않는다
-	printf("%s\n", name);
+	// scanf("%s", name); // 문자열을 입력받을때는 주소연산자를 붙이지 않는다
+	// printf("%s\n", name);
 
 	// fgets(받을배열, 최대길이, stdin);
 	// 표준입력장치로부터 문자열을 입력받아 name에 대입한다.
-	// fgets(name, 20, stdin);
-	// printf("%s\n", name)
+	fgets(name, 20, stdin);
+	int len = strlen(name); // 입력받은 문자열의 길이
+	name[len - 1] = 0; // \n 제거
+	printf("%s\n", name); // Tom Cruise\n
+
+	/*
+	입력데이터: "Tom Cruise(엔터)"
+	scanf: "Tom"
+	fgets: "Tom Cruise\n"
+	*/
+
+	// 문자열 길이: strlen
+	// 문자열 비교: strcmp
+	// 문자열 대입:
+
+	printf("sz : %s\n", sz);
+
+	// 문자열대입:
+	// sz <=== name
+	// sz = "ABCDE";
+	// 10 = "abc";
+	strcpy(sz, "ABCDE"); // sz에 "ABCDE"를 대입
+	printf("%s\n", sz);
+
+	// sz의 공간이 6byte인데 7byte의 값을 대입시도하면서...
+	// Overflow Runtime Error가 발생한다!!!
+	// strcpy(sz, "ABCDEF"); // sz에 "ABCDEF"를 대입 시도
+	// printf("%s\n", sz); // ABCDE
+
+	// syntax error(compile error)
+	// runtime error <--- debugging
+	// logical error
+
+	// 문자열 추가
+	// s = 'abc'
+	// s = s + 'def'
+	// s == 'abcdef'
+	// name [20] : 알파벳 19글자까지를 저장할 수 있다.
+	strcat(name, "plus_string"); // name = name + "plus string";
+	printf("name: %s\n", name); 
 
 
 	return 0;
---
+++ project14/project14.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <stdio.h>
+
+// 함수 : 특정 목적을 처리하기 위한 코드를 처리의 이름을 부여하여
+// 정의한 것
+
+// 함수의 정의
+void print_int(int number)
+{
+    // void 함수는 호출부에 결과를 반환하지 않는 함수이다
+    printf("정수 : %d\n", number);
+}
+
+// add함수는 반환값으 데이터 타입 정의를 int형으로 했으므로
+// 반드시 함수 내부에서 정수형 값을 반환해야 한다
+// 함수명(매개변수)
+// 매개변수: parameter, argument, 인수, 인자
+int add(int a, int b)
+{
+    int c = a + b;
+
+    // return: 호출부에 결과를 반환하면서 이 함수를 즉시 종료시킨다
+    return c;
+}
+
+void show_menu()
+{
+    printf("1. 새파일\n");
+    printf("2. 파일 저장\n");
+    printf("3. 파일 열기\n");
+    printf("x. 종료\n");
+}
+
+
+
+int main()
+{
+    // print_int() 함수의 호출
+    // 함수의 호출은 반드시!!! 먼저 정의가 되어 있어야 한다!!!
+    print_int(1);
+    print_int(2);
+    print_int(3);
+
+    int a = add(10, 5);
+    printf("%d\n", a);
+
+    return 0;
+}
